6f138997d45ecfb237c1f9ffa0cece6d70914748
FlippyNat <100854483+FlippyNat@users.noreply.github.com>
2023-09-07 17:37:30 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "employee.cspnc5ouklrd.us-east-1.rds.amazonaws.com"
+customhost = "employee.cmra6zadfm3t.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
 custombucket = "tanyanpin-employee"
-customregion = "us-east-1"
+customregion = "us-east-1b"
 
